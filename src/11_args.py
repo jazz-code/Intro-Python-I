7614e45457c176851d842c6d4002ddba380bccd4
ren.meta@gmail.com <ren.meta@gmail.com>
2019-12-03 19:38:47 -0500

    args and classes WIP

--- src/11_args.py
+++ src/11_args.py
@@ -3,7 +3,8 @@
 
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
-
+def f1(x, y):
+   return sum([x, y])
 # YOUR CODE HERE
 
 print(f1(1, 2))
@@ -11,7 +12,8 @@ print(f1(1, 2))
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*args):
+   return sum((args))
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +23,24 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+#print(f2(int(a)))    # Should print 22
+# a = [0, 1, 2]
+
+y = ''.join(map(str, a))
+print("y", y)
+z = int(y)
+print(f2(z))
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x = sum(()) + 1, y = sum(())):
+    if x:
+        return x + 1
+    else:
+        return (x, y)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
